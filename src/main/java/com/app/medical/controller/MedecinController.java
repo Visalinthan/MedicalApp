030c1408cba15a9f0d083fd7b488442ea7cead9f
Visalinthan <visalinthan@gmail.com>
2023-12-12 10:48:12 +0100

    api medecin

--- src/main/java/com/app/medical/controller/MedecinController.java
+++ src/main/java/com/app/medical/controller/MedecinController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/patient")
+@RequestMapping("api/medecin")
 public class MedecinController {
     @Autowired
     MedecinService medecinService;
