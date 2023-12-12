2ca3ac7a4369a5dfa43017be15ca3cc41541eb9e
DevAppl016 <amakhzoum5@gmail.com>
2023-12-12 14:54:29 +0100

    ajout repo et service et constroller Equipment

---
+++ src/main/java/com/app/medical/controller/exceptions/EquipmentController.java
@@ -0,0 +1,48 @@
+package com.app.medical.controller.exceptions;
+
+
+import com.app.medical.model.Equipment;
+import com.app.medical.services.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/equipment")
+public class EquipmentController {
+    @Autowired
+    EquipmentService  equipmentService;
+
+    @GetMapping("/list")
+    public List<Equipment> listOfAllEquipments() {
+        return equipmentService.list();
+    }
+
+    @GetMapping("/get/{id}")
+    public Equipment getEquipmentById(@PathVariable Long id) {
+        Optional<Equipment> appointement = equipmentService.findById(id);
+        return appointement.orElseThrow(()-> new NotFound("Le equipement avec l'id " + id + " est INTROUVABLE. "));
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Equipment> addEquipments(@RequestBody Equipment equipment) {
+
+        Equipment newEquipment = equipmentService.saveEquipment(equipment);
+
+        if(newEquipment == null) throw new AddException("Impossible d'ajouter le equipement");
+
+        return new ResponseEntity<Equipment>(equipment, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteEquipment(@PathVariable Long id) {
+
+        equipmentService.deleteEquipment(id);
+
+         return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/app/medical/services/EquipmentService.java
@@ -0,0 +1,43 @@
+package com.app.medical.services;
+
+
+import com.app.medical.model.Equipment;
+import com.app.medical.repository.EquipmentRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EquipmentService {
+    private EquipmentRepo  equipmentRepo;
+
+
+    public EquipmentService(EquipmentRepo quipmentRepo) {
+        this.equipmentRepo =equipmentRepo ;
+    }
+
+    public List<Equipment> list(){return this.equipmentRepo.findAll();}
+
+    public Optional<Equipment> findById(Long id){return this.equipmentRepo.findById(id);}
+
+    public  Equipment saveEquipment(Equipment equipment){
+        return this.equipmentRepo.save(equipment);
+    }
+
+    //méthode de suppression Rendez-vous
+    public  void  deleteEquipment(Long id) {
+
+        /*if(id != null) {
+            this.teamRepository.deleteById(id);
+        }  else   {
+            throw new TeamInvalidException("Suppression échoué :");
+        }*/
+        this.equipmentRepo.deleteById(id);
+
+    }
+
+
+
+
+}
